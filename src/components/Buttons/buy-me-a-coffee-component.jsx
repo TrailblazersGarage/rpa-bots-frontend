db128dd52953b78c9dde79398beb282f2a5d4a18
Jhony Vidal <mrjhonyvidal@gmail.com>
2022-01-07 05:23:01 +0000

    Assign value instead of empty expression for build

--- src/components/Buttons/buy-me-a-coffee-component.jsx
+++ src/components/Buttons/buy-me-a-coffee-component.jsx
@@ -5,7 +5,7 @@ const BuyMeACoffeeBtn = () => {
     return (
         <div className="pa3 center">
             <div className="w-90">
-                <a href="https://www.buymeacoffee.com/keepcoding"><img src="https://img.buymeacoffee.com/button-api/?text=Help me keep coding&emoji=&slug=keepcoding&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" alt={} /></a>
+                <a href="https://www.buymeacoffee.com/keepcoding"><img src="https://img.buymeacoffee.com/button-api/?text=Help me keep coding&emoji=&slug=keepcoding&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" alt="Welcome" /></a>
             </div>
         </div>
     );
