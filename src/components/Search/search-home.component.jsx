956e1229b591bbe5198b2885d6e001283cc9c11c
Jhony Vidal <mrjhonyvidal@gmail.com>
2022-01-09 22:28:05 +0000

    Small fix in order to pass Netlify build

--- src/components/Search/search-home.component.jsx
+++ src/components/Search/search-home.component.jsx
@@ -5,12 +5,12 @@ const SearchHome = ({ searchChange }) => {
     return (
         <div>
             <div className="ph3 center">
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">react.js</a>
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">next.js</a>
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-green">flutter</a>
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">node.js</a>
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">blockchain</a>
-                <a className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">nft</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">react.js</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">next.js</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-green">flutter</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">node.js</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">blockchain</a>
+                <a href="#" className="f5 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue">nft</a>
             </div>
             <div className='pa3'>
                 <div className='center'>
